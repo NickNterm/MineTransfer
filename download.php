077285b710b2922bfb6d245a63ee0d3c11350e90
nicknt <nikolasderm@gmail.com>
2021-02-25 00:09:29 +0200

    edit download.php

--- download.php
+++ download.php
@@ -1,27 +1,37 @@
-<?php
-$servername = "localhost";
-$username = "admin";
-$password = "admin";
-$dbname = "MineTransfer";
+<html>
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-$download = $_GET["file"];
-$sql = "SELECT * FROM Data WHERE code = '" . $download . "';";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        rename('transfers/'.$download,'transfers/'.$row['filename']);
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . $row['filename']. '"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        flush(); // Flush system output buffer
-        readfile('transfers/'.$row['filename']);
-        rename('transfers/'.$row['filename'],'transfers/'.$download);
-        die();
+<body>
+
+    <?php
+    $servername = "localhost";
+    $username = "admin";
+    $password = "admin";
+    $dbname = "MineTransfer";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $download = $_GET["file"];
+    $sql = "SELECT * FROM Data WHERE code = '" . $download . "';";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "The Message from the sender was: " . $row['message'];
+            rename('transfers/' . $download, 'transfers/' . $row['filename']);
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . $row['filename'] . '"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            flush(); // Flush system output buffer
+            readfile('transfers/' . $row['filename']);
+            rename('transfers/' . $row['filename'], 'transfers/' . $download);
+            die();
+        }
+    } else {
+        echo 'Invalid file -_-';
     }
-} else {
-    echo 'Invalid file -_-';
-}
+    ?>
+
+</body>
+
+</html>
